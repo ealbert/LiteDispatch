d9cfa83a77f1104d906d9c9d75492011ad0137d6
Enrique Albert <enrique.albert.gleiser@gmail.com>
2013-05-10 23:23:30 +0100

    Challenge 3 - Connect to Azure SQL
    Change the connection to point to the database in Azure

---
+++ LiteDispatch.Web/Migrations/DomainContextFactory.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity.Infrastructure;
+using System.Linq;
+using System.Web;
+using LiteDispatch.Domain.Mappings;
+using LiteDispatch.EF.TransManager;
+
+namespace LiteDispatch.Web.Migrations
+{
+  public class DomainContextFactory
+    : IDbContextFactory<LiteDispatchDbContext>
+  {
+    public LiteDispatchDbContext Create()
+    {
+      return new LiteDispatchDbContext(new ModelCreator());
+    }
+  }
+}
